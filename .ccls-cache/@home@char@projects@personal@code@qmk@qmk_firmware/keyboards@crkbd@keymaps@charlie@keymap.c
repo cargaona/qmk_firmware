9526a11a0b52fc219290ad79ff8e8cc7ac4abd94
charlie <carlosmartingaona@gmail.com>
2022-12-02 16:18:05 +0100

    Added more changes with 3x5 layout

--- .ccls-cache/@home@char@projects@personal@code@qmk@qmk_firmware/keyboards@crkbd@keymaps@charlie@keymap.c
+++ .ccls-cache/@home@char@projects@personal@code@qmk@qmk_firmware/keyboards@crkbd@keymaps@charlie@keymap.c
@@ -35,21 +35,21 @@ enum crkbd_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_3x6_3(             
       QK_GESC,         KC_Q,      KC_W, KC_E, KC_R,  KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,                   KC_BSPC,
-      KC_LSFT_TAB,     KC_LSFT_A, KC_S, KC_D, KC_F,  KC_G,                           KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,                  KC_QUOT,
+      KC_LSFT_TAB,     KC_LSFT_A, KC_S, KC_D, KC_F,  KC_G,                           KC_H,    KC_J,    KC_K,    KC_L, KC_QUOTE,                  KC_QUOT,
       KC_LCTL,         KC_LCTL_Z, KC_X, KC_C, KC_V,  KC_B,                           KC_N,    KC_M, KC_COMM,  KC_DOT, MT(MOD_RSFT, KC_SLSH),    MT(MOD_RSFT, KC_ENT),
-      MT(MOD_LALT,     KC_TAB),   LT(_LOWER, KC_ESC),  MT(MOD_LGUI, KC_ENT),         MT(MOD_RALT ,KC_SPC),  LT(_RAISE, KC_BSPC) , KC_QUOTE                      
+      MT(MOD_LALT,     KC_TAB),   LT(_LOWER, KC_ESC),  MT(MOD_LGUI, KC_ENT),         KC_SPC,  LT(_RAISE, KC_BSPC) , MT(MOD_RALT, KC_GRV)                      
   ),
   [_LOWER] = LAYOUT_split_3x6_3(
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  _______,
-      _______, _______, _______, _______, _______, _______,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, KC_DEL,
-      _______, _______, _______, _______, _______, _______,                      KC_PGUP, KC_HOME, KC_PGDN, KC_END,   _______, _______, 
-                                          _______, _______,  _______,            _______, MO(_ADJUST) ,KC_DEL
+      _______, _______, _______, _______, _______, _______,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_SCLN, KC_DEL,
+      _______, _______, _______, _______, _______, _______,                      KC_PGUP, KC_HOME, KC_PGDN, KC_END,   KC_PIPE, _______, 
+                                          _______, _______,  _______,            _______, MO(_ADJUST) , _______
   ),
 
   [_RAISE] = LAYOUT_split_3x6_3(
       _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
       _______, _______, _______, _______, _______, _______,                     KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,
-      _______, _______, _______, _______, _______, _______,                     KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, _______,
+      _______, _______, _______, _______, _______, _______,                     KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______ , _______,
                                           _______, MO(_ADJUST), _______,        _______, _______, _______
   ),
   
