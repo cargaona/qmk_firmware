05ebaa517e1338baa9dbecdd3348104c9f612a21
charlie <carlosmartingaona@gmail.com>
2022-12-02 23:26:42 +0100

    :eyes:

--- .ccls-cache/@home@char@projects@personal@code@qmk@qmk_firmware/keyboards@crkbd@keymaps@charlie@keymap.c
+++ .ccls-cache/@home@char@projects@personal@code@qmk@qmk_firmware/keyboards@crkbd@keymaps@charlie@keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       QK_GESC,          KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,             KC_BSPC,
       LSFT_T(KC_TAB),   KC_A,         KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,          KC_QUOT,
       KC_LCTL,          LCTL_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,                       KC_N,    KC_M,    KC_COMM, KC_DOT, RSFT_T(KC_SLSH),  RSFT_T(KC_ENT),
-                        LALT_T(KC_TAB),   LT_LWESC,     MT(MOD_LGUI, KC_ENT),                KC_SPC,  LT_RABSPC, RALT_T(KC_GRV)                      
+                        LALT_T(KC_TAB),   LT_LWESC,     MT(MOD_LGUI, KC_ENT),                KC_SPC,  MO(_RAISE), RALT_T(KC_GRV)                      
   ),
   [_LOWER] = LAYOUT_split_3x6_3(
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  _______,
