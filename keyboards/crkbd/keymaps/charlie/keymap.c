a2eb95db195066a2dbfb84b47e57efc158c6119a
charlie <carlosmartingaona@gmail.com>
2022-12-02 21:44:03 +0100

    :eyes:

--- keyboards/crkbd/keymaps/charlie/keymap.c
+++ keyboards/crkbd/keymaps/charlie/keymap.c
@@ -35,8 +35,8 @@ enum crkbd_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_3x6_3(             
       QK_GESC,         KC_Q,      KC_W, KC_E, KC_R,  KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,                   KC_BSPC,
-      KC_LSFT_TAB,     KC_LSFT_A, KC_S, KC_D, KC_F,  KC_G,                           KC_H,    KC_J,    KC_K,    KC_L, KC_QUOTE,                  KC_QUOT,
-      KC_LCTL,         KC_LCTL_Z, KC_X, KC_C, KC_V,  KC_B,                           KC_N,    KC_M, KC_COMM,  KC_DOT, MT(MOD_RSFT, KC_SLSH),    MT(MOD_RSFT, KC_ENT),
+      KC_LSFT_TAB,     KC_A, KC_S, KC_D, KC_F,  KC_G,                           KC_H,    KC_J,    KC_K,    KC_L, KC_QUOTE,                  KC_QUOT,
+      KC_LCTL,         KC_Z, KC_X, KC_C, KC_V,  KC_B,                           KC_N,    KC_M, KC_COMM,  KC_DOT, MT(MOD_RSFT, KC_SLSH),    MT(MOD_RSFT, KC_ENT),
       MT(MOD_LALT,     KC_TAB),   LT(_LOWER, KC_ESC),  MT(MOD_LGUI, KC_ENT),         KC_SPC,  LT(_RAISE, KC_BSPC) , MT(MOD_RALT, KC_GRV)                      
   ),
   [_LOWER] = LAYOUT_split_3x6_3(
