8c9d61e384678056e4afab3f025bdadc91c35c95
charlie <carlosmartingaona@gmail.com>
2022-12-03 03:35:05 +0100

    Added combos and tap dances to make 3x5 layout more usable

--- keyboards/crkbd/keymaps/charlie/keymap.c
+++ keyboards/crkbd/keymaps/charlie/keymap.c
@@ -16,6 +16,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+// TODO: C-z is not possible to do using 3x5 layout. 
+// TODO: S-/ to trigger '?' char is not possible using 3x5 layout. 
+
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
@@ -28,19 +31,57 @@ enum crkbd_layers {
     _MOUSE, 
 };
 
+enum {
+  TD_P_BSPC = 0,
+  CT_CLN
+};
+
+
+const uint16_t PROGMEM OP_BSPC_COMBO[] = {KC_O, KC_P, COMBO_END};
+const uint16_t PROGMEM OP_ESC_COMBO[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM OP_TAB_COMBO[] = {KC_A, KC_S, COMBO_END};
+const uint16_t PROGMEM OP_SFT_COMBO[] = {KC_Z, KC_X, COMBO_END};
+combo_t key_combos[COMBO_COUNT] = {
+    COMBO(OP_BSPC_COMBO, KC_BSPC),
+    COMBO(OP_ESC_COMBO, KC_ESC),
+    COMBO(OP_TAB_COMBO, KC_TAB),
+};
+
+void dance_cln_finished (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    register_code (KC_RSFT);
+    register_code (KC_SCLN);
+  } else {
+    register_code (KC_SCLN);
+  }
+}
+
+void dance_cln_reset (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    unregister_code (KC_RSFT);
+    unregister_code (KC_SCLN);
+  } else {
+    unregister_code (KC_SCLN);
+  }
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [CT_CLN] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_cln_finished, dance_cln_reset)
+};
+
 #define LT_LWESC LT(_LOWER, KC_ESC)
 #define LT_RABSPC LT(_RAISE, KC_BSPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_3x6_3(             
-      QK_GESC,          KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,             KC_BSPC,
-      LSFT_T(KC_TAB),   KC_A,         KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,          KC_QUOT,
+      QK_GESC,          KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
+      LSFT_T(KC_TAB),   KC_A,         KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,   TD(CT_CLN),       KC_QUOT,
       KC_LCTL,          LCTL_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,                       KC_N,    KC_M,    KC_COMM, KC_DOT, RSFT_T(KC_SLSH),  RSFT_T(KC_ENT),
                         LALT_T(KC_TAB),   LT_LWESC,     MT(MOD_LGUI, KC_ENT),                KC_SPC,  MO(_RAISE), RALT_T(KC_GRV)                      
   ),
   [_LOWER] = LAYOUT_split_3x6_3(
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  _______,
-      _______, _______, _______, _______, _______, _______,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_SCLN, KC_DEL,
+      _______, _______, _______, _______, _______, _______,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_QUOT, KC_DEL,
       _______, _______, _______, _______, _______, _______,                      KC_PGUP, KC_HOME, KC_PGDN, KC_END,   KC_PIPE, _______, 
                                           _______, _______,  _______,            _______, MO(_ADJUST) , _______
   ),
@@ -71,109 +112,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             _______, _______, _______,          KC_BTN1, _______, _______
   )
 };
-
-#ifdef OLED_ENABLE
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_keyboard_master()) {
-    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-  }
-  return rotation;
-}
-
-#define L_BASE 0
-#define L_LOWER 2
-#define L_RAISE 4
-#define L_ADJUST 8
-//#define TAPPING_TOGGLE 2
-
-void oled_render_layer_state(void) {
-    oled_write_P(PSTR("Layer: "), false);
-    switch (layer_state) {
-        case L_BASE:
-            oled_write_ln_P(PSTR("Default"), false);
-            break;
-        case L_LOWER:
-            oled_write_ln_P(PSTR("Lower"), false);
-            break;
-        case L_RAISE:
-            oled_write_ln_P(PSTR("Raise"), false);
-            break;
-        case L_ADJUST:
-        case L_ADJUST|L_LOWER:
-        case L_ADJUST|L_RAISE:
-        case L_ADJUST|L_LOWER|L_RAISE:
-            oled_write_ln_P(PSTR("Adjust"), false);
-            break;
-    }
-}
-
-
-char keylog_str[24] = {};
-
-const char code_to_name[60] = {
-    ' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f',
-    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-    'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\',
-    '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
-
-void set_keylog(uint16_t keycode, keyrecord_t *record) {
-  char name = ' ';
-    if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) ||
-        (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) { keycode = keycode & 0xFF; }
-  if (keycode < 60) {
-    name = code_to_name[keycode];
-  }
-
-  // update keylog
-  snprintf(keylog_str, sizeof(keylog_str), "%dx%d, k%2d : %c",
-           record->event.key.row, record->event.key.col,
-           keycode, name);
-}
-
-void oled_render_keylog(void) {
-    oled_write(keylog_str, false);
-}
-
-void render_bootmagic_status(bool status) {
-    /* Show Ctrl-Gui Swap options */
-    static const char PROGMEM logo[][2][3] = {
-        {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
-        {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
-    };
-    if (status) {
-        oled_write_ln_P(logo[0][0], false);
-        oled_write_ln_P(logo[0][1], false);
-    } else {
-        oled_write_ln_P(logo[1][0], false);
-        oled_write_ln_P(logo[1][1], false);
-    }
-}
-
-void oled_render_logo(void) {
-    static const char PROGMEM crkbd_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
-        0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
-        0};
-    oled_write_P(crkbd_logo, false);
-}
-
-bool oled_task_user(void) {
-    if (is_keyboard_master()) {
-        oled_render_layer_state();
-        oled_render_keylog();
-    } else {
-        oled_render_logo();
-    }
-    return false;
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-    set_keylog(keycode, record);
-  }
-  return true;
-}
-#endif // OLED_ENABLE
--- keyboards/crkbd/keymaps/charlie/rules.mk
+++ keyboards/crkbd/keymaps/charlie/rules.mk
@@ -3,3 +3,5 @@ RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
 OLED_ENABLE     = no
 #OLED_DRIVER     = SSD1307
 LTO_ENABLE      = yes
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
